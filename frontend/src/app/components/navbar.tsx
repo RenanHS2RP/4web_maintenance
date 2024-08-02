6cb459109c18e697fe9bcb11c85a7da5a5da0eb1
RenanHS2RP <renanoliveiratm@hotmail.com>
2024-08-02 19:50:07 -0300

    style(navbar.tsx): chaged some configurations to globas.css, navbar order and added RiTeamFill icon
    css de transparecia e cor do texto sairam do código nesta página para a de global.css, além de trocar a ordem de Manutenção e adicionar Equipes

--- frontend/src/app/components/navbar.tsx
+++ frontend/src/app/components/navbar.tsx
@@ -1,17 +1,19 @@
 import { BiCategory, BiSolidDashboard } from "react-icons/bi";
 import { FaIndustry, FaUser, FaWrench } from "react-icons/fa";
+import { RiTeamFill } from "react-icons/ri";
 
 const links = [
+  { target: "#", text: "Manutenção", icon: <FaWrench /> },
   { target: "#", text: "Dashboard", icon: <BiSolidDashboard /> },
   { target: "#", text: "Ambiente", icon: <BiCategory /> },
   { target: "#", text: "Equipamentos", icon: <FaIndustry /> },
-  { target: "#", text: "Manutenção", icon: <FaWrench /> },
   { target: "#", text: "Usuários", icon: <FaUser /> },
+  { target: "#", text: "Equipes", icon: <RiTeamFill /> },
 ];
 
 export default function NavBar() {
   return (
-    <nav className="space-y-4 flex flex-col mt-8">
+    <nav className="space-y-4 flex flex-col chgfont mt-8">
       {links.map((obj) => (
         <>
           <a
